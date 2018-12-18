114cd3bf78f9815e6f6e361c64ecc10a9ad3eb86
Michel Kuhlburger <mkuhlburger@gmail.com>
2018-12-18 12:23:36 +0100

    Créé un makefile avec cmake.

---
+++ bin/CMakeFiles/jeux.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/michel/TP/CPE_JeuxVideo/src/collidable.cpp" "/home/michel/TP/CPE_JeuxVideo/bin/CMakeFiles/jeux.dir/src/collidable.cpp.o"
+  "/home/michel/TP/CPE_JeuxVideo/src/events.cpp" "/home/michel/TP/CPE_JeuxVideo/bin/CMakeFiles/jeux.dir/src/events.cpp.o"
+  "/home/michel/TP/CPE_JeuxVideo/src/game.cpp" "/home/michel/TP/CPE_JeuxVideo/bin/CMakeFiles/jeux.dir/src/game.cpp.o"
+  "/home/michel/TP/CPE_JeuxVideo/src/main.cpp" "/home/michel/TP/CPE_JeuxVideo/bin/CMakeFiles/jeux.dir/src/main.cpp.o"
+  "/home/michel/TP/CPE_JeuxVideo/src/player.cpp" "/home/michel/TP/CPE_JeuxVideo/bin/CMakeFiles/jeux.dir/src/player.cpp.o"
+  "/home/michel/TP/CPE_JeuxVideo/src/sceneManager.cpp" "/home/michel/TP/CPE_JeuxVideo/bin/CMakeFiles/jeux.dir/src/sceneManager.cpp.o"
+  "/home/michel/TP/CPE_JeuxVideo/src/tir.cpp" "/home/michel/TP/CPE_JeuxVideo/bin/CMakeFiles/jeux.dir/src/tir.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CORRECTION"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ bin/CMakeFiles/jeux.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jeux.dir/src/collidable.cpp.o"
+  "CMakeFiles/jeux.dir/src/events.cpp.o"
+  "CMakeFiles/jeux.dir/src/game.cpp.o"
+  "CMakeFiles/jeux.dir/src/main.cpp.o"
+  "CMakeFiles/jeux.dir/src/player.cpp.o"
+  "CMakeFiles/jeux.dir/src/sceneManager.cpp.o"
+  "CMakeFiles/jeux.dir/src/tir.cpp.o"
+  "jeux.pdb"
+  "jeux"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jeux.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
